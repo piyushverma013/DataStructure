adddad2ca6bdbfbf8cfd09fae82adc7cc6d0d770
Piyush Verma <piyushverma013@gmail.com>
2021-01-28 21:54:29 +0530

    Add files via upload

---
+++ Array/Sort012.java
@@ -0,0 +1,72 @@
+import java.util.*;
+public class Sort012 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter size of the array: ");
+		int n = obj.nextInt();
+		int arr[] = new int[n];
+		System.out.println("Enter array elements: ");
+		for(int i = 0; i < arr.length; i++)
+		{
+			arr[i] = obj.nextInt();
+		}
+		
+		System.out.println("Array elements are: ");
+		printArray(arr, n);
+		
+		System.out.println();
+		sortArray012(arr, n);
+		
+		System.out.println("Sorted array is: ");
+		printArray(arr, n);
+	}
+	
+	
+	// function to sort the array
+	static void sortArray012(int arr[], int n)
+	{
+		int low = 0, mid = 0, high = n - 1;
+		int temp;
+		while(mid <= high)
+		{
+			switch(arr[mid]) {
+			case 0 : 
+			{
+					temp = arr[low];
+					arr[low] = arr[mid];
+					arr[mid] = temp;
+					low++;
+					mid++;	
+					break;
+			}
+			case 1 : 
+			{
+					mid++;
+					break;
+			}
+			
+			case 2:
+			{
+				temp = arr[high];
+				arr[high] = arr[mid];
+				arr[mid] = temp;
+				high--;
+				break;
+			}
+			}
+		}
+	}
+	
+	// function to print the array
+	static void printArray(int arr[], int n)
+	{
+		for(int i = 0; i < n; i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	}
