d18047e1a21b538f776b888fa3601a0ee38785a1
Piyush Verma <piyushverma013@gmail.com>
2021-01-28 22:00:55 +0530

    Update Sort012.java

--- Array/Sort012.java
+++ Array/Sort012.java
@@ -30,32 +30,28 @@ public class Sort012 {
 	{
 		int low = 0, mid = 0, high = n - 1;
 		int temp;
+		
 		while(mid <= high)
 		{
-			switch(arr[mid]) {
-			case 0 : 
+			if(arr[mid] == 0) 
 			{
 					temp = arr[low];
 					arr[low] = arr[mid];
 					arr[mid] = temp;
 					low++;
 					mid++;	
-					break;
 			}
-			case 1 : 
+			else if(arr[mid] == 1) 
 			{
 					mid++;
-					break;
 			}
 			
-			case 2:
+			else
 			{
 				temp = arr[high];
 				arr[high] = arr[mid];
 				arr[mid] = temp;
 				high--;
-				break;
-			}
 			}
 		}
 	}
