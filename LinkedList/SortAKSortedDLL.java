4dbb1d290c4f5d8aca23fb00d46d0e36d738775c
Piyush Verma <25269267+piyushverma013@users.noreply.github.com>
2021-08-11 10:10:56 +0530

    Update SortAKSortedDLL.java

--- LinkedList/SortAKSortedDLL.java
+++ LinkedList/SortAKSortedDLL.java
@@ -1,8 +1,7 @@
 // Given a doubly linked list containing n nodes, where each node is at most k away from its target position in the list. 
 // The problem is to sort the given doubly linked list. 
 // For example, let us consider k is 2, a node at position 7 in the sorted doubly linked list, can be at positions 5, 6, 7, 8, 9 in the given doubly linked list.
-// Input: 3 <-> 6 <-> 2 <-> 12 <-> 56 <-> 8   
-// k = 2
+// Input: 3 <-> 6 <-> 2 <-> 12 <-> 56 <-> 8   // k = 2
 // Output: 2 <-> 3 <-> 6 <-> 8 <-> 12 <-> 56
 
 class compareNode implements Comparator<Node>
